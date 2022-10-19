d4d785785a8e8256aa6c39592899a092cf7c2b7a
Olivier Samin <oliviersamin@gmail.com>
2022-10-19 09:04:34 +0100

    update files location to be abble to build docker images with the script

--- create_docker_containers.py
+++ create_docker_containers.py
@@ -6,7 +6,6 @@ It starts from scratch
 import os
 
 PATH_BASE_TELEGRAM = "/home/ospgd/Andolina/colegio_andolino"
-PATH_RPI_DOCKER = "Rpi-docker"
 DOCKERFILE_BASE_IMAGE_FILE = "Dockerfile_to_create_base_image"
 DOCKERFILE_BASE_IMAGE_NAME = "base_image_for_telegram:v1.0"
 DOCKERFILE_TELEGRAM_SERVER_IMAGE_FILE = "Dockerfile_to_create_telegram_basics"
@@ -14,7 +13,8 @@ DOCKERFILE_TELEGRAM_SERVER_IMAGE_NAME = "telegram_server:v1.0"
 DOCKER_COMPOSE_FILE = "telegram.yaml"
 
 commands = [
-    "cd {}".format(PATH_BASE_TELEGRAM + "/" + PATH_RPI_DOCKER),
+    "docker rmi -f {}".format(DOCKERFILE_BASE_IMAGE_NAME),
+    "cd {}".format(PATH_BASE_TELEGRAM),
     "docker build -f {} -t {} .".format(DOCKERFILE_BASE_IMAGE_FILE, DOCKERFILE_BASE_IMAGE_NAME),
     "docker build -f {} -t {} .".format(DOCKERFILE_TELEGRAM_SERVER_IMAGE_FILE, DOCKERFILE_TELEGRAM_SERVER_IMAGE_NAME),
     "docker-compose -f {} up chat commands".format(DOCKER_COMPOSE_FILE),
