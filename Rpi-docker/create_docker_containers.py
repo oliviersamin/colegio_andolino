287b778df03ec1bd9d666e388fe7a0524e977414
Olivier SAMIN <oliviersamin@gmail.com>
2022-10-19 09:16:01 +0200

    script to simplify the creation of docker images and launch containers

---
+++ Rpi-docker/create_docker_containers.py
@@ -0,0 +1,26 @@
+"""
+This script automatize all the steps to create docker images for telegram RPI 3 B+
+It starts from scratch
+"""
+
+import os
+
+PATH_BASE_TELEGRAM = "/home/ospgd/Andolina/colegio_andolino"
+PATH_RPI_DOCKER = "Rpi-docker"
+DOCKERFILE_BASE_IMAGE_FILE = "Dockerfile_to_create_base_image"
+DOCKERFILE_BASE_IMAGE_NAME = "base_image_for_telegram:v1.0"
+DOCKERFILE_TELEGRAM_SERVER_IMAGE_FILE = "Dockerfile_to_create_telegram_basics"
+DOCKERFILE_TELEGRAM_SERVER_IMAGE_NAME = "telegram_server:v1.0"
+DOCKER_COMPOSE_FILE = "telegram.yaml"
+
+commands = [
+    "cd {}".format(PATH_BASE_TELEGRAM + "/" + PATH_RPI_DOCKER),
+    "docker build -f {} -t {} .".format(DOCKERFILE_BASE_IMAGE_FILE, DOCKERFILE_BASE_IMAGE_NAME),
+    "docker build -f {} -t {} .".format(DOCKERFILE_TELEGRAM_SERVER_IMAGE_FILE, DOCKERFILE_TELEGRAM_SERVER_IMAGE_NAME),
+    "docker-compose -f {} up chat commands".format(DOCKER_COMPOSE_FILE),
+
+]
+
+
+for command in commands:
+    os.system(command)
