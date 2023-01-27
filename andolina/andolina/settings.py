d27fb40493e58417e0a5d7c1434579a60deaea24
Maximilian Harz <maxharz@gmail.com>
2023-01-27 11:29:27 +0100

    Read secret key from environment; add .env.example file

--- andolina/andolina/settings.py
+++ andolina/andolina/settings.py
@@ -1,3 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 """
 Django settings for andolina project.
 
@@ -19,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--vs2cdh-i*^*^!_8kn9i@q&-*y2r84exz=&!if&vafvz(cmmjw'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
